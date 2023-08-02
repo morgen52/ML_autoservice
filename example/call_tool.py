a3a5e8d709aea6a8fc740f0add0e52dd5ebace7f
1271989088@qq.com <1271989008@qq.com>
2023-08-02 13:28:30 +0800

    fix readme

--- example/call_tool.py
+++ example/call_tool.py
@@ -7,6 +7,10 @@ import json
 
 base_url = "http://0.0.0.0:8000"
 
+'''
+输入：zip文件路径
+输出：一个json字典，包括{"call": 一个简单的模型服务调用示例, "url": 模型服务的url, "name": 模型服务的名称}
+'''
 def create(filepath="1.zip"):
     url= f"{base_url}/create"
     # upload a zip file to the tool
@@ -20,24 +24,40 @@ def create(filepath="1.zip"):
             f.write(content["call"])
         return content["name"]
 
+'''
+输入：创建时返回的模型服务名称
+输出：无
+'''
 def delete(name):
     url = f"{base_url}/delete"
     content = {"name": name}
     r = requests.post(url, json=content)
     print(r.text)
     
+'''
+输入：创建时返回的模型服务名称
+输出：一个json字典，包括{"call": 一个简单的模型服务调用示例, "url": 模型服务的url, "name": 模型服务的名称}
+'''
 def get(name):
     url = f"{base_url}/get"
     content = {"name": name}
     r = requests.post(url, json=content)
     print(r.text)
 
+'''
+输入：无
+输出：一个json字典，包括{"message": 所有目前已有服务的列表}
+'''
 def list():
     url = f"{base_url}/list"
     r = requests.get(url)
     print(r.text)
     return json.loads(r.text)
 
+'''
+输入：创建时返回的模型服务名称，zip文件路径
+输出：保留原有服务的url不变，返回一个json字典，包括{"call": 一个简单的模型服务调用示例, "url": 模型服务的url, "name": 模型服务的名称}
+'''
 def update(name, filepath="2.zip"):
     url = f"{base_url}/update"
     files = {'file': open(filepath, 'rb')}
@@ -57,13 +77,10 @@ if __name__ == "__main__":
     delete(name)
     list()
 
-    # name = create()
-    # name = list()["message"][0]
-    # update(name)
-    
-
-
-
-
+    name = create()
+    name = list()["message"][0]
+    update(name)
+    delete(name)
+    list()
 
 
